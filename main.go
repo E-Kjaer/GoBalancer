7284b67a4f8ed580917b8cba5e1c4ea89fbd88ae
Emil Kjær Fuglsang <emilkf95@gmail.com>
2024-12-21 21:16:27 +0100

    Config file
    Added support for config file with servers, algorithm and port

--- main.go
+++ main.go
@@ -8,20 +8,19 @@ import (
 	"os"
 )
 
+var Port int
+var Algorithm string
 var Servers []Server
 var DelegateMap = map[string]Scheduler{}
 
 func main() {
-	loadTestServers()
-	DelegateMap["r"] = NewRandomScheduler(Servers)
-	DelegateMap["rr"] = NewRoundRobinScheduler(Servers)
-	DelegateMap["wrr"] = NewWeightedRoundRobinScheduler(Servers)
-
-	var port int32 = 3333
+	loadConfig()
+	loadSchedulers()
+	//var port int32 = 3333
 	http.HandleFunc("/", handleTraffic)
 
-	fmt.Println(fmt.Sprintf("Load Balancer listening on port [:%d]", port))
-	err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), nil)
+	fmt.Println(fmt.Sprintf("Load Balancer listening on port [:%d]", Port))
+	err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", Port), nil)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
@@ -35,22 +34,32 @@ func loadConfig() {
 	}
 	var j map[string]interface{}
 	err = json.Unmarshal(dat, &j)
-	servers := j["servers"].([]interface{})[0]
-	fmt.Println(servers)
+
+	Port = int(j["lb_port"].(float64))
+
+	Algorithm = j["lb_algo"].(string)
+
+	servers := j["be_servers"].([]interface{})
+	for _, element := range servers {
+		obj := element.(map[string]interface{})
+		server := Server{
+			Name:     obj["name"].(string),
+			Host:     obj["host"].(string),
+			Port:     uint16(obj["port"].(float64)),
+			Priority: uint8(obj["priority"].(float64)),
+		}
+		Servers = append(Servers, server)
+	}
 }
 
-func loadTestServers() {
-	server1 := Server{"", "127.0.0.1", 5555, 2}
-	server2 := Server{"", "127.0.0.1", 6666, 4}
-	server3 := Server{"", "127.0.0.1", 7777, 8}
-	Servers = append(Servers, server1)
-	Servers = append(Servers, server2)
-	Servers = append(Servers, server3)
+func loadSchedulers() {
+	DelegateMap["rand"] = NewRandomScheduler(Servers)
+	DelegateMap["rr"] = NewRoundRobinScheduler(Servers)
+	DelegateMap["wrr"] = NewWeightedRoundRobinScheduler(Servers)
 }
 
 func handleTraffic(w http.ResponseWriter, r *http.Request) {
-	routingMethod := "wrr"
-	res := DelegateMap[routingMethod].Delegate(r)
+	res := DelegateMap[Algorithm].Delegate(r)
 	defer res.Body.Close()
 	bytes, err := io.ReadAll(res.Body)
 	if err != nil {
